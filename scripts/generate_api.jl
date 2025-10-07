00f821f8f9eed8bcfb29bcacd9ece14311eb5f10
Matt Bauman <mbauman@juliacomputing.com>
2025-10-07 15:09:53 -0400

    use Pkg

--- scripts/generate_api.jl
+++ scripts/generate_api.jl
@@ -1,15 +1,16 @@
 using TOML: TOML
 using JSON: JSON
-using DataStructures: OrderedDict
+using DataStructures: DataStructures, OrderedDict
+using Pkg: Pkg, Registry
 
 function main()
     allversions = TOML.parsefile(joinpath(@__DIR__, "..", "registration_dates.toml"))
-    general = only(filter(x->x.uuid==Base.UUID("23338594-aafe-5451-b93e-139f81909106"), Pkg.Registry.reachable_registries()))
+    general = only(filter(x->x.uuid==Base.UUID("23338594-aafe-5451-b93e-139f81909106"), Registry.reachable_registries()))
     root = mkpath(joinpath(@__DIR__, "..", "webroot", "api"))
     for (pkg, versions) in allversions
         pkgdir = mkpath(joinpath(root, pkg))
         JSON.json(joinpath(pkgdir, "versions.json"), sort(OrderedDict(versions), by=VersionNumber))
-        JSON.json(joinpath(pkgdir, "info.json"), (; name=pkg, uuid=string(only(Pkg.Registry.uuids_from_name(general, pkg)))))
+        JSON.json(joinpath(pkgdir, "info.json"), (; name=pkg, uuid=string(only(Registry.uuids_from_name(general, pkg)))))
     end
 end
 
