b4e6ab2ea43309242173ea0a22e5f2290b900151
Matt Bauman <mbauman@juliacomputing.com>
2025-10-07 15:34:47 -0400

    skip removed packages

--- scripts/generate_api.jl
+++ scripts/generate_api.jl
@@ -8,9 +8,11 @@ function main()
     general = only(filter(x->x.uuid==Base.UUID("23338594-aafe-5451-b93e-139f81909106"), Registry.reachable_registries()))
     root = mkpath(joinpath(@__DIR__, "..", "webroot", "api"))
     for (pkg, versions) in allversions
+        uuid = Registry.uuids_from_name(general, pkg)
+        isempty(uuid) && continue # These are packages that have since been removed from the registry; mostly the cappening
         pkgdir = mkpath(joinpath(root, pkg))
         JSON.json(joinpath(pkgdir, "versions.json"), sort(OrderedDict(versions), by=VersionNumber))
-        JSON.json(joinpath(pkgdir, "info.json"), (; name=pkg, uuid=string(only(Registry.uuids_from_name(general, pkg)))))
+        JSON.json(joinpath(pkgdir, "info.json"), (; name=pkg, uuid=string(only(uuid))))
     end
 end
 
